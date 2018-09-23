f6ac6c0e117ac279a766d9b0899e766558068cb8
Alexander <neishkasha.alexandr@gmail.com>
2018-09-23 21:20:42 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Ads Board</title>
+
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <main class="col-8">
+
+                </main>
+                <aside class="col-4">
+                    test2
+                </aside>
+            </div>
+        </div>
+    </body>
+</html>
