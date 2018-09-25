3f3e40e701349d0e80ec96aa3f9af6bb5563cd5f
Neishkasha Alexander <neishkasha.alexandr@gmail.com>
2018-09-25 17:17:12 +0300

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,23 +14,32 @@ class DatabaseSeeder extends Seeder
     {
         //users
         DB::table('users')->insert([
-            'name' => 'User',
-            'password' => Hash::make('userpass')
+            'name' => 'User1',
+            'password' => Hash::make('user1pass'),
+            'created_at' =>  \Carbon\Carbon::now()->toDateTimeString()
         ]);
         DB::table('users')->insert([
             'name' => 'User2',
-            'password' => Hash::make('user2pass')
+            'password' => Hash::make('user2pass'),
+            'created_at' =>  \Carbon\Carbon::now()->toDateTimeString()
         ]);
         DB::table('users')->insert([
             'name' => 'User3',
-            'password' => Hash::make('user3pass')
+            'password' => Hash::make('user3pass'),
+            'created_at' =>  \Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' =>  \Carbon\Carbon::now()->toDateTimeString()
         ]);
 
+        $lipsum = new joshtronic\LoremIpsum();
         //Ads
-        DB::table('ads')->insert([
-            'title' => 'Progressively network resource sucking technology',
-            'description' => 'Monotonectally maintain client-focused value rather than transparent manufactured products. Appropriately.',
-            'author_name' => 'User'
-        ]);
+        for ($i = 1; $i <= 3; $i++)
+            for ($j = 0; $j < 3; $j++)
+                DB::table('ads')->insert([
+                    'title' => $lipsum->words(5),
+                    'description' => $lipsum->words(15),
+                    'author_name' => "User{$i}",
+                    'created_at' =>  \Carbon\Carbon::now()->toDateTimeString(),
+                    'updated_at' =>  \Carbon\Carbon::now()->toDateTimeString()
+                ]);
     }
 }
